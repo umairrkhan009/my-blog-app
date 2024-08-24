27751770578deee388a7003f33d6e88d9db4e887
umairrkhan009 <157248715+umairrkhan009@users.noreply.github.com>
2024-08-24 19:26:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 import express from 'express';
-import  bodyParser  from 'body-parser';
+import bodyParser from 'body-parser';
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import methodOverride from 'method-override';
-import exp from 'constants';
-import { title } from 'process';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
-
 
 const app = express();
 const port = 3000;
@@ -16,10 +12,7 @@ var blogs = [];
 var response = [];
 const year = new Date();
 
-
-
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.use(express.static("public"));
@@ -29,7 +22,7 @@ app.get("/", (req, res) => {
         blogs: blogs,
         year,
     });
-})
+});
 
 app.post("/submit", (req, res) => {
     const blogObj = {
@@ -37,14 +30,18 @@ app.post("/submit", (req, res) => {
         "title": req.body["title"],
         "desc": req.body["blog"],
         "createdAt": new Date().toLocaleString(),
-    }
+    };
     blogs.push(blogObj);
     console.log(blogs);
-    res.redirect("/");
-})
+    
+    // Encode the title when redirecting
+    const encodedTitle = encodeURIComponent(blogObj.title);
+    res.redirect(`/blogs/${encodedTitle}`);
+});
 
 app.get("/blogs/:blogID", (req, res) => {
-    const postTitle = req.params.blogID.toLowerCase();
+    // Decoding the URL parameter to get the correct blog title
+    const postTitle = decodeURIComponent(req.params.blogID).toLowerCase();
     const blog = blogs.find(blog => blog.title.toLowerCase() === postTitle);
 
     if (blog) {
@@ -55,11 +52,12 @@ app.get("/blogs/:blogID", (req, res) => {
             createdAt: blog.createdAt,
             auth: blog.name,
         });
-    } 
+    }
 });
 
 app.patch('/blogs/update/:blogID', (req, res) => {
-    const blogID = req.params.blogID;
+    // Decoding the URL parameter to get the correct blog title
+    const blogID = decodeURIComponent(req.params.blogID);
     const { newTitle, newDesc } = req.body;
 
     let blog = blogs.find(blog => blogID === blog.title);
@@ -67,78 +65,69 @@ app.patch('/blogs/update/:blogID', (req, res) => {
     if (blog) {
         blog.title = newTitle || blog.title;
         blog.desc = newDesc || blog.desc;
-        res.redirect(`/blogs/${blog.title}`);
-        console.log(blogs);
         
-    } 
+        // Encoding the updated title for URL
+        const encodedTitle = encodeURIComponent(blog.title);
+        res.redirect(`/blogs/${encodedTitle}`);
+        console.log(blogs);
+    }
 });
 
+app.get("/blogs/edit/:blogID", (req, res) => {
+    // Decoding the URL parameter to get the correct blog title
+    const blogTitle = decodeURIComponent(req.params.blogID);
+    const blog = blogs.find(blog => blogTitle === blog.title);
 
-app.get("/blogs/edit/:blogID", (req, res)=>{
-    const blogTitle = req.params.blogID;
-    const blog = blogs.find(blog => blogTitle===blog.title);
-    
-    if(blog){
-        res.render("edit.ejs",{
+    if (blog) {
+        res.render("edit.ejs", {
             blogTitle,
             blogContent: blog.desc,
             year,
-        })
+        });
     }
-})  
+});
 
-
-
-
-app.post("/blogs/delete/:blogID", (req, res) =>{
-    let blogTitle = req.params.blogID;
-    blogs = blogs.filter(blog => blog.title!== blogTitle);
+app.post("/blogs/delete/:blogID", (req, res) => {
+    // Decoding the URL parameter to get the correct blog title
+    let blogTitle = decodeURIComponent(req.params.blogID);
+    blogs = blogs.filter(blog => blog.title !== blogTitle);
 
     res.redirect('/');
-
     res.status(204).send();
-})
+});
 
 app.get("/about.ejs", (req, res) => {
     res.render("about.ejs", {
         year,
     });
-})
+});
 
 app.get("/create.ejs", (req, res) => {
     res.render("create.ejs", {
         year,
     });
-})
+});
 
 app.get("/contact.ejs", (req, res) => {
-    res.render("contact.ejs",{
+    res.render("contact.ejs", {
         response,
         year,
     });
-    response.length=0;
-})
-
+    response.length = 0;
+});
 
 app.post("/contact/submit", (req, res) => {
     const responseObj = {
         "name": req.body.name,
         "email": req.body.email,
         "message": req.body.message,
-    }
+    };
     response.push(responseObj);
     console.log(response);
     res.status(202).send;
     res.redirect("/contact.ejs");
-    
-})
+});
 
-
-
-
-
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server is live on ${port}`);
-})
-
+});
